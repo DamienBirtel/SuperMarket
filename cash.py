0c343fdd59eac1158f4856672bbcccad6d100299
DamienBirtel <damien.birtel@gmail.com>
2021-11-17 00:59:20 +0100

    added : - return dictionary with strings as keys - conditions to shorten the algo

--- cash.py
+++ cash.py
@@ -13,21 +13,52 @@ class _Coins:
 
 
 class CashManager:
-    # CashManager takes a tuple of integers as parameters which represents the coins and notes we are able to return
-    # We sort it in reversed order to allow an easier manipulation later
-    def __init__(self, availableCoins=(10, 5, 2)):
-        self.coinsAndNotes = sorted(availableCoins, reverse=True)
+    # CashManager takes a dictionary of numbers represented with integers as key and strings as values
+    def __init__(self, availableCoins={10:"ten", 5:"five", 2:"two"}):
+        self.coinsAndNotes = []
+        self.availableCoins = availableCoins
+        self.returnDictionary = {}
+        for key, value in availableCoins.items():
+            self.coinsAndNotes.append(key)
+            self.returnDictionary[value] = 0
+        self.coinsAndNotes = sorted(self.coinsAndNotes, reverse=True)
 
+    def _modulo(self, amountToReturn, coin):
+        if amountToReturn % coin != 0:
+            return None
+        change = amountToReturn // coin
+        self.returnDictionary[self.availableCoins[coin]] = change
+        return self.returnDictionary
+
+    def _contains_odd_coin(self):
+        for coin in self.coinsAndNotes:
+            if coin % 2 == 0:
+                return True
+        return False
+
+    def _return_dictionary(self, dict):
+        for key, value in dict.items():
+            self.returnDictionary[self.availableCoins[key]] = value
+        return self.returnDictionary
 
     # change takes an integer amountToReturn as a paramter and returns a dictionary where 
     # the keys are the types of coins and the values are the amount of coins to add up to amountToReturn
     # or returns None if it can't find an exact combination of coins to get to amountToReturn
     def change(self, amountToReturn=0):
 
+        if len(self.coinsAndNotes) == 0 :
+            return None
+
+        if len(self.coinsAndNotes) == 1:
+            return self._modulo(amountToReturn, self.coinsAndNotes[0])
+
+        if amountToReturn % 2 != 0 and not self._contains_odd_coin():
+            return None
+
         # To find which coins to use to have the smallest amount of coins for amountToReturn we first
         # need to find every smallest amount of coins for every number up to amountToReturn so
         # we create a list of _Coins which will hold information for that
-        combinations = [_Coins(self.coinsAndNotes) for x in range(amountToReturn + 1)]
+        combinations = [_Coins(self.coinsAndNotes) for _ in range(amountToReturn + 1)]
 
         # We set the first amount of coins to 0 as we need 0 coin to give back 0 change
         combinations[0].amount = 0
@@ -60,7 +91,7 @@ class CashManager:
             return None
 
         # we return the dictionary holding the information about which amount of which coins to use
-        return combinations[amountToReturn].types
+        return self._return_dictionary(combinations[amountToReturn].types)
 
                     
         
